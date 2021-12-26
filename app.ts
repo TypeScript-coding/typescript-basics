1f48c88864bbeb169171c565428b35885ad17683
Adam Morsi <adam.morsi@yahoo.com>
2021-12-26 16:29:31 +0200

    add function with arguments

--- app.ts
+++ app.ts
@@ -1,1 +1,8 @@
-console.log('Time to get started...');
+function add(n1:number, n2: number) {
+    return n1 + n2;
+}
+
+const number1 = 5,
+    number2 = 2.8;
+
+console.log(add(number1, number2));
