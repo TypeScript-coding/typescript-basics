d39447d821c963d9bfa53ceb347c6184f73fe5f0
Adam Morsi <adam.morsi@yahoo.com>
2021-12-28 12:10:32 +0200

    use function as type

--- app.ts
+++ app.ts
@@ -1,3 +1,4 @@
+//function return type
 function add(n1: number, n2: number): number {
     return n1 + n2;
 }
@@ -9,3 +10,12 @@ const add2 = (n1: number, n2: number) : number => {
 function printResult(num: number): void {
     console.log('Result: ', num);
 }
+
+printResult(add(5, 12));
+
+//functions as types
+let combineValues: (a: number, b: number) => number;
+
+combineValues = add;
+
+console.log(combineValues(8, 8));
