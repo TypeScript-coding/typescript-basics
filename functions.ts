1e20e2284024d0ed9659a25e85540a396482e8d5
Adam Morsi <adam.morsi@yahoo.com>
2021-12-28 14:27:12 +0200

    add tsconfig.json file

--- functions.ts
+++ functions.ts
@@ -1,7 +1,7 @@
 //function return type
-function add(n1: number, n2: number): number {
+/*function add(n1: number, n2: number): number {
     return n1 + n2;
-}
+}*/
 
 const add2 = (n1: number, n2: number) : number => {
     return n1 + n2;
@@ -11,12 +11,12 @@ function printResult(num: number): void {
     console.log('Result: ', num);
 }
 
-printResult(add(5, 12));
+printResult(add2(5, 12));
 
 //functions as types
 let combineValues: (a: number, b: number) => number;
 
-combineValues = add;
+combineValues = add2;
 
 console.log(combineValues(8, 8));
 
