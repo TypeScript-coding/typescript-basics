200629ef1de09cccd47f7860142b85c3950a969a
Adam Morsi <adammor@amdocs.com>
2021-12-29 14:45:32 +0200

    use private key

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,29 @@
 class Department {
     name: string;
+    private employees: string[] = [];
 
     constructor(n: string) {
         this.name = n;
+    }
+
+    describe(this: Department) {
+        console.log(`Department: ${this.name}`);
+    }
+
+    addEmployee(employee: string) {
+        this.employees.push(employee);
+    }
+
+    printEmployeeInformation() {
+        console.log('Number of employees: ',this.employees.length);
+        console.log('Employees list', this.employees);
     }
 }
 
 const accounting = new Department('Accounting');
 
-console.log(accounting);
+accounting.addEmployee('Adam');
+accounting.addEmployee('Bassant');
+
+accounting.describe();
+accounting.printEmployeeInformation();
