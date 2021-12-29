6bae10009677366fe31f4d91a85261d1ee75aaf9
Adam Morsi <adammor@amdocs.com>
2021-12-29 15:08:56 +0200

    use inheritance

--- src/app.ts
+++ src/app.ts
@@ -18,10 +18,29 @@ class Department {
     }
 }
 
+//inheritance
+class ITDepartment extends Department {
+    constructor(id: string, public admins: string[]) {
+        super(id, 'IT');
+    }
+}
+
+//accounting
 const accounting = new Department('d1', 'Accounting');
+console.log('%c Accounting department', 'color: red; font-size: 20px;', accounting);
 
 accounting.addEmployee('Adam');
 accounting.addEmployee('Bassant');
 
 accounting.describe();
 accounting.printEmployeeInformation();
+
+//IT
+const it = new ITDepartment('d2', ['Adam', 'Regina']);
+console.log('%c IT department', 'color: red; font-size: 20px;', it);
+
+it.addEmployee('Adam');
+it.addEmployee('Bassant');
+
+it.describe();
+it.printEmployeeInformation();
