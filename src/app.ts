32df78996193694b2ad6ba6c8e4a27ad606181c5
Adam Morsi <adam.morsi@yahoo.com>
2021-12-31 14:57:11 +0200

    use descriminated unions

--- src/app.ts
+++ src/app.ts
@@ -22,3 +22,33 @@ type Numeric = number | boolean;
 type Universal = Generic & Numeric;
 
 const n1: Universal = 3;
+
+//discriminated unions
+interface Bird {
+    type: 'bird',
+    flyingSpeed: number,
+}
+
+interface Horse {
+    type: 'horse',
+    runningSpeed: number,
+}
+
+type Animal = Bird | Horse;
+
+function moveAnimal(animal: Animal) {
+    let speed;
+    switch (animal.type) {
+        case "bird":
+            speed = animal.flyingSpeed;
+            break;
+        case "horse":
+            speed = animal.runningSpeed;
+            break;
+        default:
+            console.log('Not an animal');
+    }
+    console.log(`Moving at speed ${speed}`);
+}
+
+moveAnimal({type: "bird", flyingSpeed: 50});
