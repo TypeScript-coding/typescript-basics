e3d92871248b17d0246d8634e497286576282975
Adam Morsi <adam.morsi@yahoo.com>
2021-12-31 14:18:25 +0200

    use intersection types with union types

--- src/app.ts
+++ src/app.ts
@@ -15,3 +15,10 @@ const el: ElevatedEmployee = {
     privileges: ['create-server'],
     startDate: new Date(),
 }
+
+type Generic =  string | number;
+type Numeric = number | boolean;
+
+type Universal = Generic & Numeric;
+
+const n1: Universal = 3;
