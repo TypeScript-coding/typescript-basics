4a85c03ac95bf433789d7f9539f63f8d3bdc96e7
Adam Morsi <adam.morsi@yahoo.com>
2022-01-02 14:26:30 +0200

    assign more decorators to a class

--- src/app.ts
+++ src/app.ts
@@ -13,13 +13,14 @@ class Person {
     }
 }
 
-const pers = new Person();
+/*const pers = new Person();
 
-console.log(pers);
+console.log(pers);*/
 
 /************** decorator factories ***************/
 //example1
 const Logger2 = (logString: string) => (constructor: Function) => {
+    console.log('Logger2');
     console.log(logString);
     console.log(constructor);
 }
@@ -27,13 +28,14 @@ const Logger2 = (logString: string) => (constructor: Function) => {
 //example2
 const withTemplate = (template: string, hookId: string) => (_: Function) => {
     const element = document.getElementById(hookId);
+    console.log('Rendering template');
 
     if(element) {
         element.innerHTML = template;
     }
 }
 
-// @Logger2('LOGGING - PERSON')
+@Logger2('LOGGING - PERSON')
 @withTemplate('<h1>My person object</h1>', 'app')
 class AnotherPerson {
     name = 'Adam2';
