3fedb316df493f80226011436f1a4e3d5fbcd938
Adam Morsi <adam.morsi@yahoo.com>
2022-01-02 12:11:09 +0200

    create another generic function

--- src/app.ts
+++ src/app.ts
@@ -33,3 +33,22 @@ const initialState = {
 };
 
 console.log(updateObject(initialState, {show: true}));
+
+//example3
+interface Lengthy {
+    length: number
+}
+
+function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
+    let descriptionText = 'Got no value';
+
+    if(element.length === 1) {
+        descriptionText = 'Got 1 element';
+    } else if(element.length > 1) {
+        descriptionText = `Got ${element.length} elements`;
+    }
+    return [element, descriptionText];
+}
+
+console.log(countAndDescribe(('Hi there!')));
+console.log(countAndDescribe((['Sports', 'Cooking'])));
