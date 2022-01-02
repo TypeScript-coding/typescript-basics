e9c92ab80418eb442089a38c96c7df3e96d372d3
Adam Morsi <adam.morsi@yahoo.com>
2022-01-02 12:38:46 +0200

    use generic classes

--- src/app.ts
+++ src/app.ts
@@ -59,3 +59,31 @@ function extractAndConvert<T extends object, U extends keyof T>(obj: T, key: U) 
 }
 
 console.log(extractAndConvert({name: 'Adam'}, 'name'));
+
+/************** generic classes ***************/
+class DataStorage<T extends string | number | boolean> {
+    private data: T[] = [];
+
+    addItem(item: T) {
+        this.data.push(item);
+    }
+
+    removeItem(item: T) {
+        if(this.data.splice(this.data.indexOf(item) === -1) {
+            return;
+        }
+        this.data.splice(this.data.indexOf(item), 1);
+    }
+
+    getItems() {
+        return [...this.data];
+    }
+}
+
+const textStorage = new DataStorage<string>();
+textStorage.addItem('Adam');
+textStorage.addItem('Max');
+textStorage.removeItem('Max');
+console.log(textStorage.getItems());
+
+const numberStorage = new DataStorage<number>();
